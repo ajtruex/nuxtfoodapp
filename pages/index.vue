3ab05e7443841399c7ce6a9248a9f2770b3d137e
Andrew Truex <andrewtruex@gmail.com>
2020-11-10 09:08:30 -0500

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="container">
+    <AppHeader />
+    <AppRestaurantInfo />
+    <!-- <AppFooter /> -->
+  </main>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader.vue"
+// import AppFooter from "@/components/AppFooter.vue"
+import AppRestaurantInfo from "@/components/AppRestaurantInfo.vue"
+
+export default {
+  components: {
+    AppHeader,
+    AppRestaurantInfo,
+    // AppFooter,
+  },
+}
+</script>
+
+<style></style>
